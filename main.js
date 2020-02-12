5e6456f62cbeabc4e9af2caa9527c6822a921072
Andres Rodriguez <andres.rodriguez@opratel.com>
2020-02-12 10:22:59 -0300

    0.1.2

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ let switchBool = true
 let colorStayTime = 10
 let transitionTime = colorStayTime / 10
 
-export default async function (elementId, colorList, stateTime) {
+module.exports = async function (elementId, colorList, stateTime) {
     data = colorList
     colorStayTime = stateTime
     await searchElement(elementId)
