6e21a2849c64961decfbe579504ee4508269203d
Andres Rodriguez <andres.rodriguez@opratel.com>
2020-02-12 17:00:25 -0300

    0.1.6 first working version

--- main.js
+++ main.js
@@ -1,82 +1,85 @@
 /* Data */
 let data = []
 let element = null // Reference element
-let elements = {} // Created Elements
+const elements = {} // Created Elements
 let switchBool = true
+const backgroundClass = '__background'
+const firstBackground = '__first_background'
+const secondBackground = '__second_background'
 
 /* Config */
 let colorStayTime = 10
-let transitionTime = colorStayTime / 10
+const transitionTime = colorStayTime / 10
 
 export default async function (elementId, colorList, stateTime) {
-    data = colorList
-    colorStayTime = stateTime
-    await searchElement(elementId)
-    await createBackgroundElements()
-    await getElementsInScreen()
-    await createCSSClasses(elementId, colorList)
-    startAnimations()
+  data = colorList
+  colorStayTime = stateTime
+  await searchElement(elementId)
+  await createBackgroundElements()
+  await getElementsInScreen()
+  await createCSSClasses(elementId, colorList)
+  startAnimations()
 }
 
 // Search container element
 function searchElement (elementId) {
-    return new Promise((resolve, reject) => {
-        try {
-            element = document.getElementById(elementId)
-            resolve()
-        } catch {
-            reject('Cannot find Element: ' + elementId)
-        }
-    })
+  return new Promise((resolve, reject) => {
+    try {
+      element = document.getElementById(elementId)
+      resolve()
+    } catch {
+      reject(new Error('Cannot find Element: ' + elementId))
+    }
+  })
 }
 
 // Create background to render colors
 function createBackgroundElements () {
-    return new Promise((resolve, reject) => {
-        try {
-            if (!element) reject('Cannot find Element')
+  return new Promise((resolve, reject) => {
+    try {
+      if (!element) reject(new Error('Cannot find Element'))
 
-            let div1 = document.createElement('div')
-            let div2 = document.createElement('div')
+      const div1 = document.createElement('div')
+      const div2 = document.createElement('div')
 
-            div1.setAttribute('class', '__background')
-            div1.setAttribute('id', '__first-background')
-            div2.setAttribute('class', '__background')
-            div2.setAttribute('id', '__second-background')
-            element.appendChild(div1)
-            element.appendChild(div2)
+      div1.setAttribute('class', backgroundClass)
+      div1.setAttribute('id', firstBackground)
+      div2.setAttribute('class', backgroundClass)
+      div2.setAttribute('id', secondBackground)
+      element.appendChild(div1)
+      element.appendChild(div2)
 
-            resolve()
-        } catch {
-            reject('Error trying to create background elements')
-        }
-    })
+      resolve()
+    } catch {
+      reject(new Error('Error trying to create background elements'))
+    }
+  })
 }
 
 // Get backgrounds elements in screen
 function getElementsInScreen () {
-    return new Promise((resolve, reject) => {
-        elements.firstBackground = document.getElementById('__first-background')
-        elements.secondBackground = document.getElementById('__second-background')
-        if (elements.firstBackground && elements.secondBackground) resolve()
-        if (!elements.firstBackground || !elements.secondBackground) reject('Error finding elements')
-    })
+  return new Promise((resolve, reject) => {
+    elements.firstBackground = document.getElementById(firstBackground)
+    elements.secondBackground = document.getElementById(secondBackground)
+    if (elements.firstBackground && elements.secondBackground) resolve()
+    if (!elements.firstBackground || !elements.secondBackground) reject(new Error('Error finding elements'))
+  })
 }
 
 // Creates CSS Classes from JSON Data
 function createCSSClasses (elementId, colorList) {
-    return new Promise((resolve, reject) => {
-        try {
-            let style = document.createElement('style')
-            style.type = 'text/css'
-            colorList.forEach((styles, index, array) => {
-                style.innerHTML += `.__background_${index} { background: ${styles.color} !important; }`
-            })
-            style.innerHTML += `
+  return new Promise((resolve, reject) => {
+    try {
+      const style = document.createElement('style')
+      style.type = 'text/css'
+      colorList.forEach((styles, index, array) => {
+        style.innerHTML += `.${backgroundClass}_${index} { background: ${styles.color} !important; }`
+      })
+      style.innerHTML += `
                 #${elementId} {
                     position: relative;
                 }
-                .__background {
+                .${backgroundClass} {
                     transition: opacity ${transitionTime}s linear;
                     top: 0;
                     left: 0;
@@ -85,58 +88,55 @@ function createCSSClasses (elementId, colorList) {
                     position: absolute;
                     opacity: 0;
                 }
-                .__background.show {
+                .${backgroundClass}.show {
                     opacity: 1 !important;
                 }
             `
-            document.getElementsByTagName('head')[0].appendChild(style);
-            resolve()
-        } catch {
-            reject('Cannot create CSS Classes')
-        }
-    })
+      document.getElementsByTagName('head')[0].appendChild(style)
+      resolve()
+    } catch {
+      reject(new Error('Cannot create CSS Classes'))
+    }
+  })
 }
 
 function startAnimations () {
-    let {firstBackground} = elements
+  const { firstBackground } = elements
 
-    firstBackground.className += ` __background_${getRandomBG()} show` // Set first color befre loop start
-    backgroundsLoop()
+  firstBackground.className += ` ${backgroundClass}_${getRandomBG()} show` // Set first color befre loop start
+  backgroundsLoop()
 }
 
 function backgroundsLoop () {
-    let {firstBackground, secondBackground} = elements
+  const { firstBackground, secondBackground } = elements
 
-    // Initalize varibles
-    let first = firstBackground
-    let second = secondBackground
+  // Initalize varibles
+  let first = firstBackground
+  let second = secondBackground
 
-    // Invert values to reverse loop
-    if (!switchBool) {
-        first = secondBackground
-        second = firstBackground
-    }
+  // Invert values to reverse loop
+  if (!switchBool) {
+    first = secondBackground
+    second = firstBackground
+  }
 
-    // Set a new color in the hidden background
-    second.className += ` __background_${getRandomBG()}`
+  // Set a new color in the hidden background
+  second.className += ` ${backgroundClass}_${getRandomBG()}`
 
-    // Wait stay color time to switch between colors
+  // Wait stay color time to switch between colors
+  setTimeout(() => {
+    first.className = first.className.replace('show', '')
+    second.className += ' show'
+
+    // Remove color from the hidden one, change boolean & call loop again
     setTimeout(() => {
-        first.className = first.className.replace('show', '')
-        second.className += ` show`
-
-        // Remove color from the hidden one, change boolean & call loop again
-        setTimeout(() => {
-            first.className = '__background'
-            switchBool = !switchBool
-            backgroundsLoop()
-        }, transitionTime * 1000)
-
-    }, (colorStayTime) * 1000)
-
-
+      first.className = backgroundClass
+      switchBool = !switchBool
+      backgroundsLoop()
+    }, transitionTime * 1000)
+  }, (colorStayTime) * 1000)
 }
 
 function getRandomBG () {
-    return parseInt(Math.random() * data.length)
+  return parseInt(Math.random() * data.length)
 }
