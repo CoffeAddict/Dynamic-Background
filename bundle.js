4cc7775c822beadda0db8947b4c8f45be53d13f0
Andres Rodriguez <andres.rodriguez@opratel.com>
2020-02-12 10:41:06 -0300

    0.1.3

--- bundle.js
+++ bundle.js
@@ -1,80 +1,83 @@
-'use strict';
+(function (factory) {
+    typeof define === 'function' && define.amd ? define(factory) :
+    factory();
+}((function () { 'use strict';
 
-/* Data */
-let data = [];
-let element = null; // Reference element
-let elements = {}; // Created Elements
-let switchBool = true;
+    /* Data */
+    let data = [];
+    let element = null; // Reference element
+    let elements = {}; // Created Elements
+    let switchBool = true;
 
-/* Config */
-let colorStayTime = 10;
-let transitionTime = colorStayTime / 10;
+    /* Config */
+    let colorStayTime = 10;
+    let transitionTime = colorStayTime / 10;
 
-async function main (elementId, colorList, stateTime) {
-    data = colorList;
-    colorStayTime = stateTime;
-    await searchElement(elementId);
-    await createBackgroundElements();
-    await getElementsInScreen();
-    await createCSSClasses(elementId, colorList);
-    startAnimations();
-}
+    module.exports = async function (elementId, colorList, stateTime) {
+        data = colorList;
+        colorStayTime = stateTime;
+        await searchElement(elementId);
+        await createBackgroundElements();
+        await getElementsInScreen();
+        await createCSSClasses(elementId, colorList);
+        startAnimations();
+    };
 
-// Search container element
-function searchElement (elementId) {
-    return new Promise((resolve, reject) => {
-        try {
-            element = document.getElementById(elementId);
-            resolve();
-        } catch {
-            reject('Cannot find Element: ' + elementId);
-        }
-    })
-}
+    // Search container element
+    function searchElement (elementId) {
+        return new Promise((resolve, reject) => {
+            try {
+                element = document.getElementById(elementId);
+                resolve();
+            } catch {
+                reject('Cannot find Element: ' + elementId);
+            }
+        })
+    }
 
-// Create background to render colors
-function createBackgroundElements () {
-    return new Promise((resolve, reject) => {
-        try {
-            if (!element) reject('Cannot find Element');
+    // Create background to render colors
+    function createBackgroundElements () {
+        return new Promise((resolve, reject) => {
+            try {
+                if (!element) reject('Cannot find Element');
 
-            let div1 = document.createElement('div');
-            let div2 = document.createElement('div');
+                let div1 = document.createElement('div');
+                let div2 = document.createElement('div');
 
-            div1.setAttribute('class', '__background');
-            div1.setAttribute('id', '__first-background');
-            div2.setAttribute('class', '__background');
-            div2.setAttribute('id', '__second-background');
-            element.appendChild(div1);
-            element.appendChild(div2);
+                div1.setAttribute('class', '__background');
+                div1.setAttribute('id', '__first-background');
+                div2.setAttribute('class', '__background');
+                div2.setAttribute('id', '__second-background');
+                element.appendChild(div1);
+                element.appendChild(div2);
 
-            resolve();
-        } catch {
-            reject('Error trying to create background elements');
-        }
-    })
-}
+                resolve();
+            } catch {
+                reject('Error trying to create background elements');
+            }
+        })
+    }
 
-// Get backgrounds elements in screen
-function getElementsInScreen () {
-    return new Promise((resolve, reject) => {
-        elements.firstBackground = document.getElementById('__first-background');
-        elements.secondBackground = document.getElementById('__second-background');
-        if (elements.firstBackground && elements.secondBackground) resolve();
-        if (!elements.firstBackground || !elements.secondBackground) reject('Error finding elements');
-    })
-}
+    // Get backgrounds elements in screen
+    function getElementsInScreen () {
+        return new Promise((resolve, reject) => {
+            elements.firstBackground = document.getElementById('__first-background');
+            elements.secondBackground = document.getElementById('__second-background');
+            if (elements.firstBackground && elements.secondBackground) resolve();
+            if (!elements.firstBackground || !elements.secondBackground) reject('Error finding elements');
+        })
+    }
 
-// Creates CSS Classes from JSON Data
-function createCSSClasses (elementId, colorList) {
-    return new Promise((resolve, reject) => {
-        try {
-            let style = document.createElement('style');
-            style.type = 'text/css';
-            colorList.forEach((styles, index, array) => {
-                style.innerHTML += `.__background_${index} { background: ${styles.color} !important; }`;
-            });
-            style.innerHTML += `
+    // Creates CSS Classes from JSON Data
+    function createCSSClasses (elementId, colorList) {
+        return new Promise((resolve, reject) => {
+            try {
+                let style = document.createElement('style');
+                style.type = 'text/css';
+                colorList.forEach((styles, index, array) => {
+                    style.innerHTML += `.__background_${index} { background: ${styles.color} !important; }`;
+                });
+                style.innerHTML += `
                 #${elementId} {
                     position: relative;
                 }
@@ -91,56 +94,56 @@ function createCSSClasses (elementId, colorList) {
                     opacity: 1 !important;
                 }
             `;
-            document.getElementsByTagName('head')[0].appendChild(style);
-            resolve();
-        } catch {
-            reject('Cannot create CSS Classes');
-        }
-    })
-}
-
-function startAnimations () {
-    let {firstBackground} = elements;
-
-    firstBackground.className += ` __background_${getRandomBG()} show`; // Set first color befre loop start
-    backgroundsLoop();
-}
-
-function backgroundsLoop () {
-    let {firstBackground, secondBackground} = elements;
-
-    // Initalize varibles
-    let first = firstBackground;
-    let second = secondBackground;
-
-    // Invert values to reverse loop
-    if (!switchBool) {
-        first = secondBackground;
-        second = firstBackground;
+                document.getElementsByTagName('head')[0].appendChild(style);
+                resolve();
+            } catch {
+                reject('Cannot create CSS Classes');
+            }
+        })
     }
 
-    // Set a new color in the hidden background
-    second.className += ` __background_${getRandomBG()}`;
+    function startAnimations () {
+        let {firstBackground} = elements;
 
-    // Wait stay color time to switch between colors
-    setTimeout(() => {
-        first.className = first.className.replace('show', '');
-        second.className += ` show`;
+        firstBackground.className += ` __background_${getRandomBG()} show`; // Set first color befre loop start
+        backgroundsLoop();
+    }
 
-        // Remove color from the hidden one, change boolean & call loop again
+    function backgroundsLoop () {
+        let {firstBackground, secondBackground} = elements;
+
+        // Initalize varibles
+        let first = firstBackground;
+        let second = secondBackground;
+
+        // Invert values to reverse loop
+        if (!switchBool) {
+            first = secondBackground;
+            second = firstBackground;
+        }
+
+        // Set a new color in the hidden background
+        second.className += ` __background_${getRandomBG()}`;
+
+        // Wait stay color time to switch between colors
         setTimeout(() => {
-            first.className = '__background';
-            switchBool = !switchBool;
-            backgroundsLoop();
-        }, transitionTime * 1000);
+            first.className = first.className.replace('show', '');
+            second.className += ` show`;
 
-    }, (colorStayTime) * 1000);
+            // Remove color from the hidden one, change boolean & call loop again
+            setTimeout(() => {
+                first.className = '__background';
+                switchBool = !switchBool;
+                backgroundsLoop();
+            }, transitionTime * 1000);
+
+        }, (colorStayTime) * 1000);
 
 
-}
+    }
 
-function getRandomBG () {
-    return parseInt(Math.random() * data.length)
-}
+    function getRandomBG () {
+        return parseInt(Math.random() * data.length)
+    }
 
-module.exports = main;
+})));
